5689c2381f47091586209804d7fc43cb5c85b68e
akabane25 <unconfigured@null.spigotmc.org>
2017-04-13 10:09:04 -0700

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('UP');
+})
+
+bot.login('MzAyMTIyNTAyNjA2MjkwOTQ1.C9E9MQ.OtUyDyqcCZFn2OZ1oIFtg8sZ2os');
